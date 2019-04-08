2de31ae3ec412fca0ad647a0037d00403a2f0788
Ryan Grunest <ryangrunest45@gmail.com>
2019-04-08 12:24:55 -0700

    first version done

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -9,14 +9,36 @@ const connection = mysql.createConnection({
     database: 'bamazon'
   });
 
-placeOrder = (idVal, pCount) => {
+placeOrder = (idVal, pCount, initCount) => {
     idVal = parseInt(idVal);
     pCount = parseInt(pCount);
-    console.log(idVal, pCount);
-    if (isNaN(idVal) || isNaN(pCount)) {
+    initCount = parseInt(initCount);
+    console.log(idVal, pCount, initCount);
+    if (isNaN(idVal) || isNaN(pCount) || isNaN(initCount)) {
         console.log('Must enter a number! Returning to main program...');
         initialPrompts();
     };
+    // app checks to see if product is available
+    if ((initCount - pCount) >= 0) {
+        newCount = initCount - pCount;
+        console.log(pCount);
+        connection.query('UPDATE products SET stock_quantity = ? WHERE item_id = ?', [newCount, idVal], function (e, r, f) {
+            if (e) console.log(e);
+                // console.log('changed ' + r.changedRows + ' rows');
+            });
+            connection.query({
+                sql: 'SELECT * FROM `products` WHERE `item_id` = ?',
+                values: [idVal]
+            }, (error, results, fields) => {
+                if (error) throw console.log(error);
+                console.log(`Purchased ${pCount} ${results[0].product_name}s for a total of $${pCount * parseInt(results[0].price)}`);
+                connection.end();
+            });
+    } else if ((initCount - pCount) < 0)  {
+        console.log('Not enough quantity, please try again with a lower amount');
+        initialPrompts();
+    }
+    
 };
 
 initialPrompts = () => {
@@ -32,7 +54,14 @@ initialPrompts = () => {
             message: 'Please enter the amount you would like to purchase:'
         }
     ]).then(answers => {
-        placeOrder(answers.idVal, answers.purchaseCount);
+        connection.query({
+            sql: 'SELECT * FROM `products` WHERE `item_id` = ?',
+            values: [answers.idVal]
+        }, (error, results, fields) => {
+            if (error) throw console.log(error);
+            let initCount = results[0].stock_quantity;
+            placeOrder(answers.idVal, answers.purchaseCount, initCount);
+        });
     });
 }
 
@@ -47,16 +76,24 @@ main = () => {
             // error will be an Error if one occurred during the query
             if (error) console.log(error);
             // results will contain the results of the query
-            console.log(results, fields);
-            // fields will contain information about the returned results fields (if any)
+            console.log('Items For Sale:');
+            console.log('----------------------------------------------');
+            console.log('| ITEM ID | ITEM NAME | DEPARTMENT | PRICE |');
+            console.log('----------------------------------------------');
+            for (var i = 0; i < results.length; i++) {
+                console.log(`| ${results[i].item_id} | ${results[i].product_name} | ${results[i].department_name} | $${results[i].price} |`);
+                console.log('----------------------------------------------');
+            };
+            console.log('| ITEM ID | ITEM NAME | DEPARTMENT | PRICE |');
+            console.log('----------------------------------------------');
           });
-        
-        console.log('connected as id ' + connection.threadId);
+
         // then prompts the user with two messages: what they want to buy and how many
-        initialPrompts();    
+        setTimeout(initialPrompts, 1000);
     
-        connection.end();
+
     });
+    // connection.end();
 }
 
 
@@ -64,7 +101,7 @@ main = () => {
 
 
 
-// app checks to see if product is available
+
 // if not, log to the user
 
 // if  it  does...
